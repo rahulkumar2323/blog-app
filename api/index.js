c356937b4a54c9f9107eb85fb4cb9af5bc678c27
rahulkumar <107376311+flashpogu@users.noreply.github.com>
2024-03-14 10:07:43 +0530

    add a middleware function to handle error

--- api/index.js
+++ api/index.js
@@ -19,3 +19,13 @@ app.use("/api/auth", authRoutes);
 app.listen(3000, () => {
   console.log("Server is running at 3000");
 });
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
