f339cf649bef2809fc030a67d0ccd06dac1bed97
rahulkumar <107376311+flashpogu@users.noreply.github.com>
2024-03-18 14:56:45 +0530

    complete post page func

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ import FooterCom from "./components/Footer";
 import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute";
 import CreatePost from "./pages/CreatePost";
 import UpdatePost from "./pages/UpdatePost";
+import PostPage from "./pages/PostPage";
 
 export default function App() {
   return (
@@ -29,6 +30,7 @@ export default function App() {
           <Route path="/update-post/:postId" element={<UpdatePost />}></Route>
         </Route>
         <Route path="/projects" element={<Projects />}></Route>
+        <Route path="/post/:postSlug" element={<PostPage />}></Route>
       </Routes>
       <FooterCom />
     </BrowserRouter>
