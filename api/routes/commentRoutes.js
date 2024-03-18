c6c46a7d7902470b6acce283ef4b0ad24a08007e
rahulkumar <107376311+flashpogu@users.noreply.github.com>
2024-03-18 23:45:52 +0530

    add edit func to comment

--- api/routes/commentRoutes.js
+++ api/routes/commentRoutes.js
@@ -4,6 +4,7 @@ import { createComment } from "../controlllers/commentController.js";
 import {
   getPostComments,
   likeComment,
+  editComment,
 } from "../controlllers/commentController.js";
 
 const router = express.Router();
@@ -11,5 +12,6 @@ const router = express.Router();
 router.post("/create", verifyToken, createComment);
 router.get("/getPostComments/:postId", getPostComments);
 router.put("/likeComment/:commentId", verifyToken, likeComment);
+router.put("/editComment/:commentId", verifyToken, editComment);
 
 export default router;
