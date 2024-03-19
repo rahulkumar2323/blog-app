f8f11747299e978d56309982f0ef35ed9fe2a01c
rahulkumar <107376311+flashpogu@users.noreply.github.com>
2024-03-19 13:46:48 +0530

    complete about and project page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,3 +1,31 @@
 export default function About() {
-  return <div>about</div>;
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font-semibold text-center my-7">
+            About Flash Blog
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Beyond your personal journey, outline the mission and values that
+              guide your blog. Whether you aim to educate, entertain, inspire,
+              or provoke thought, articulate the purpose behind your content.
+              Highlight any unique perspectives or approaches you bring to your
+              niche, offering readers a compelling reason to engage with your
+              blog over others.
+            </p>
+            <p>
+              Lastly, invite your readers to join you on your blogging journey.
+              Encourage them to subscribe, follow your social media channels,
+              and engage with your content through comments and shares. By
+              fostering a sense of community, you not only deepen the connection
+              with your audience but also create a platform where meaningful
+              conversations can thrive.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,3 +1,24 @@
+import CallToAction from "../components/CallToAction";
+
 export default function Projects() {
-  return <div>project</div>;
+  return (
+    <div className="min-h-screen max-w-2xl mx-auto flex justify-center flex-col gap-6 p-3">
+      <h1 className="text-3xl font-semibold">Projects</h1>
+      <p className="text-md text-gray-500">
+        In the realm of your project page, every pixel holds the promise of
+        innovation and progress. From the initial spark of inspiration to the
+        meticulous execution, your project is a testament to creativity and
+        determination. As lines of code intertwine with artistic design, and
+        data flows seamlessly through intricate algorithms, a masterpiece takes
+        shape. Each element, meticulously crafted, serves as a building block in
+        the edifice of your vision, poised to leave an indelible mark on the
+        landscape of possibility. With every click, tap, or swipe, your project
+        invites users into a world of discovery, where boundaries blur, and
+        imagination knows no limits. Embrace the journey, for within these
+        digital corridors lies the potential to redefine what is possible and
+        shape the future that awaits.
+      </p>
+      <CallToAction />
+    </div>
+  );
 }
